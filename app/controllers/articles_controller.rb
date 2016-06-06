4d2e70ff268ba2cc74f6c6872f35905ddeba3930
noraworld <noraworld@mail.com>
2016-06-06 18:18:34 +0100

    :sparkles: Authenticate user when editing and deleting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['LOGIN_NAME'], password: ENV['LOGIN_PASSWORD'], except: [:index, :show]
+
   def index
     @articles = Article.all
   end
